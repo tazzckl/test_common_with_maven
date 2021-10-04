3b76c95523574f6b723bef2f7eb559a71b919329
ckl <123>
2021-10-04 12:56:10 +0800

    aa

---
+++ src/main/java/p1/Main.java
@@ -0,0 +1,15 @@
+package p1;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s1 = scanner.next();
+        String s2 = scanner.next();
+        int[][] arr = new int[s1.length() * s2.length()][s1.length() + s2.length()];
+        for(int i = s1.length();i >= 1;i--){
+            int a = s1.charAt(i - 1) - '0';
+        }
+    }
+}
---
+++ src/main/java/p1/Solution.java
@@ -0,0 +1,21 @@
+package p1;
+
+import org.junit.Test;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = null;
+        Scanner scanner = new Scanner(System.in);
+        s = scanner.next();
+
+    }
+    @Test
+    public void t1(){
+
+    }
+}
